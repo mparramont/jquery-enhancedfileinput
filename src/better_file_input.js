81d5a581ea07050365eef395e0f397ddcf1bb2e2
Miguel Parramón <miguel.parramon@simfy.com>
2012-11-28 11:51:15 +0100

    now adding a wrapper to the input to enchance. Added specs.

--- src/better_file_input.js
+++ src/better_file_input.js
@@ -1,10 +1,13 @@
 function better_file_input_for(input_id, initial_content){
-  var l = $('#'+input_id).get(0);
+  var original_input = $('#'+input_id).get(0);
+
+  // wrapper for better_input
+  $(original_input).wrap("<div class='better_input_wrapper'>");
 
   // html for the better_input
-  $(l).parent().append("\
-    <div class='better_input'>\
-      <input id='better_file_input_for_"+input_id+"' class='fakeinput'>\
+  $('.better_input_wrapper').append("\
+    <div class='better_input_container'>\
+      <input id='better_file_input_for_"+input_id+"' class='better_file_input'>\
       <img src='img/select.png'>\
     </div>"
   );
@@ -15,8 +18,8 @@ function better_file_input_for(input_id, initial_content){
     $(better_file_input).val(filename(initial_content));
 
   // transfer the original input's value to better_file_input when it changes
-  l.relatedElement = better_file_input;
-  l.onchange = preserve_value;
+  original_input.relatedElement = better_file_input;
+  original_input.onchange = preserve_value;
 
   //styling to hide the original input and show the enhanced one
   $('.file_input_container').css('position', 'relative');
@@ -25,7 +28,7 @@ function better_file_input_for(input_id, initial_content){
     'opacity': '0',
     'z-index': '2'
   });
-  $('.better_input').css({
+  $('.better_input_container').css({
     'position': 'absolute',
     'top': '0',
     'left': '0',
